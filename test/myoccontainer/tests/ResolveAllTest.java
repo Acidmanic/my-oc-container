6b09675d419b5ed3081c56f6685390e55d151bdb
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2018-05-28 00:12:54 +0430

    Add animals model objects for test

---
+++ test/myoccontainer/tests/ResolveAllTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myoccontainer.tests;
+
+import com.acidmanic.utility.myoccontainer.Resolver;
+import com.acidmanic.utility.myoccontainer.configuration.ConfigurationFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import myoccontainer.models.BlueCarBody;
+import myoccontainer.models.Body;
+import myoccontainer.models.Car;
+import myoccontainer.models.CarMotor;
+import myoccontainer.models.Electrics;
+import myoccontainer.models.FastElectrics;
+import myoccontainer.models.HeavySilanders;
+import myoccontainer.models.Motor;
+import myoccontainer.models.Silanders;
+import myoccontainer.models.SportWheel;
+import myoccontainer.models.Wheel;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author diego
+ */
+public class ManualRegister {
+    
+    
+    
+    private final Resolver resolver = new Resolver();
+
+    public ManualRegister() {
+    
+        resolver.register(Car.class, Car.class);
+        resolver.register(Body.class, BlueCarBody.class);
+        resolver.register(Wheel.class, SportWheel.class);
+        resolver.register(Silanders.class, HeavySilanders.class);
+        resolver.register(Electrics.class, FastElectrics.class);
+        resolver.register(Motor.class, CarMotor.class);
+    
+    }
+    
+    
+    
+    @Test
+    public void resolveCar(){
+        try {
+            Car car = (Car) resolver.resolve(Car.class);
+            car.print();
+            Assert.assertTrue(true);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            Assert.fail(ex.getMessage());
+        }
+        
+    }
+    
+    @Test
+    public void testSave(){
+        try {
+            ConfigurationFile.save("config.config", resolver.getRegisteredDependancies());
+            Assert.assertTrue(true);
+        } catch (Exception ex) {
+            Logger.getLogger(ManualRegister.class.getName()).log(Level.SEVERE, null, ex);
+            Assert.fail();
+        }
+    }
+    
+    
+    
+}
